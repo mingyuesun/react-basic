0676ed55f5cbac3875a96a6f3ed5e83540d2c835
mingyuesun <smy_cating@foxmail.com>
2022-06-03 18:48:36 +0800

    8.4 render-props HOC

--- src/index.js
+++ src/index.js
@@ -1,32 +1,35 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-class MouseTracker extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {x: 0, y: 0}
-  }
-  mouseMove = (event) => {
-    this.setState({
-      x: event.clientX,
-      y: event.clientY
-    })
-  }
-  render() {
-    return (
-      <div onMouseMove={this.mouseMove}>
-        {this.props.render(this.state)}
-      </div>
-    )
+function withTracker(OldComponent) {
+  return class MouseTracker extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {x: 0, y: 0}
+    }
+    mouseMove = (event) => {
+      this.setState({
+        x: event.clientX,
+        y: event.clientY
+      })
+    }
+    render() {
+      return (
+        <div onMouseMove={this.mouseMove}>
+          <OldComponent {...this.state}/>
+        </div>
+      )
+    }
   }
 }
 
-ReactDOM.render(<MouseTracker render={
-  props => (
+function Show(props) {
+  return (
     <React.Fragment>
       <h1>鼠标位置</h1>
       <p>鼠标当前位置：{props.x}, {props.y}</p>
     </React.Fragment>
   )
-}>
-</MouseTracker>, document.getElementById("root"))
+}
+let HighShow = withTracker(Show)
+ReactDOM.render(<HighShow/>, document.getElementById("root"))
